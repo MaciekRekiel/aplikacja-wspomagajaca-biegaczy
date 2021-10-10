d04f84ad1f61a612fb9e6ce5596db7ec61e6f394
Maciej Rekiel <maciekrekiel@gmail.com>
2021-10-10 15:49:57 +0200

    Update (#1)

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import HomeScreen from "./src/screens/HomeScreen";
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "Home",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
